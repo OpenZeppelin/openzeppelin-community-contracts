0e7d04049a77ce0fcbc3de5e4266ee0921b91122
Ernesto García <ernestognw@gmail.com>
2024-09-02 22:14:32 -0600

    Apply suggestions

--- contracts/crosschain/axelar/AxelarGatewayIncoming.sol
+++ contracts/crosschain/axelar/AxelarGatewayIncoming.sol
@@ -5,10 +5,16 @@ pragma solidity ^0.8.0;
 import {IAxelarGateway} from "@axelar-network/axelar-cgp-solidity/interfaces/IAxelarGateway.sol";
 import {IGatewayIncomingPassive} from "../IGatewayIncomingPassive.sol";
 import {IGatewayIncoming} from "../IGatewayIncoming.sol";
+import {IGatewayReceiver} from "../IGatewayReceiver.sol";
+import {AxelarCAIP2Equivalence} from "./AxelarCAIP2Equivalence.sol";
+import {AxelarExecutable} from "@axelar-network/axelar-gmp-sdk-solidity/executable/AxelarExecutable.sol";
 
-abstract contract AxelarGatewayIncoming is IGatewayIncoming, IGatewayIncomingPassive {
-    IAxelarGateway public immutable gateway;
-
+abstract contract AxelarGatewayIncoming is
+    AxelarExecutable,
+    AxelarCAIP2Equivalence,
+    IGatewayIncoming,
+    IGatewayIncomingPassive
+{
     function validateReceivedMessage(
         bytes32 messageId,
         string calldata srcChain, // CAIP-2 chain ID
@@ -19,7 +25,7 @@ abstract contract AxelarGatewayIncoming is IGatewayIncoming, IGatewayIncomingPas
         if (!_isValidReceivedMessage(messageId, srcChain, srcAccount, payload, attributes)) {
             revert GatewayIncomingPassiveInvalidMessage(messageId);
         }
-        emit MessageExecuted(messageId);
+        _execute(string(fromCAIP2(destChain)), srcAccount, payload);
     }
 
     function _isValidReceivedMessage(
@@ -32,13 +38,9 @@ abstract contract AxelarGatewayIncoming is IGatewayIncoming, IGatewayIncomingPas
         return gateway.validateContractCall(messageId, srcChain, srcAccount, keccak256(payload));
     }
 
-    function _execute(
-        bytes32 messageId,
-        string calldata srcChain,
-        string calldata srcAccount,
-        bytes calldata payload,
-        bytes calldata attributes
-    ) internal {
-        // messageId
+    function _execute(string calldata srcChain, string calldata srcAccount, bytes calldata payload) internal virtual {
+        (address destination, bytes memory data) = abi.decode(payload, (address, bytes));
+        IGatewayReceiver(destination).receiveMessage(keccak256(data), sourceChain, sourceAddress, data, "");
+        emit MessageExecuted(keccak256(data)); // What to use if we can't reconstruct the message?
     }
 }
