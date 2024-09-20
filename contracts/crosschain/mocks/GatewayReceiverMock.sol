b2ac19d64ef557093bf8fa3c888e1075e8053840
Hadrien Croubois <hadrien.croubois@gmail.com>
2024-09-20 14:22:50 +0200

    up

--- contracts/crosschain/mocks/GatewayReceiverMock.sol
+++ contracts/crosschain/mocks/GatewayReceiverMock.sol
@@ -8,7 +8,7 @@ import {IGatewayReceiver} from "../interfaces/IGatewayReceiver.sol";
 contract GatewayReceiverMock is IGatewayReceiver {
     address public immutable GATEWAY;
 
-    event MessageReceived(bytes gatewayData, string srcChain, string srcAccount, bytes payload, bytes[] attributes);
+    event MessageReceived(bytes gatewayMessageKey, string source, string sender, bytes payload, bytes[] attributes);
 
     constructor(address _gateway) {
         GATEWAY = _gateway;
@@ -19,28 +19,28 @@ contract GatewayReceiverMock is IGatewayReceiver {
     }
 
     function receiveMessage(
-        address gatewayAddr,
-        bytes calldata gatewayData,
-        string calldata srcChain,
-        string calldata srcAccount,
+        address gateway,
+        bytes calldata gatewayMessageKey,
+        string calldata source,
+        string calldata sender,
         bytes calldata payload,
         bytes[] calldata attributes
     ) public payable {
         if (isGateway(msg.sender)) {
             // Active mode
             // no extra check
-        } else if (isGateway(gatewayAddr)) {
+        } else if (isGateway(gateway)) {
             // Passive mode
-            IGatewayDestinationPassive(gatewayAddr).validateReceivedMessage(
-                gatewayData,
-                srcChain,
-                srcAccount,
+            IGatewayDestinationPassive(gateway).validateReceivedMessage(
+                gatewayMessageKey,
+                source,
+                sender,
                 payload,
                 attributes
             );
         } else {
             revert("invalid call");
         }
-        emit MessageReceived(gatewayData, srcChain, srcAccount, payload, attributes);
+        emit MessageReceived(gatewayMessageKey, source, sender, payload, attributes);
     }
 }
