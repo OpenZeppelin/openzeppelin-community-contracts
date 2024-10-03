a23c05175139c9b8af7b17b09bd459b06aca93cf
cairo <cairoeth@protonmail.com>
2024-10-03 11:10:16 -0700

    Apply suggestions from code review
    Co-authored-by: Ernesto García <ernestognw@gmail.com>

--- contracts/token/ERC20/extensions/ERC20Blocklist.sol
+++ contracts/token/ERC20/extensions/ERC20Blocklist.sol
@@ -13,9 +13,6 @@ import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
  * be able to execute transfers or approvals to other entities to operation on their behalf after
  * {_blockUser} is called. Similarly, the account can operate again after calling {_unblockUser}.
  *
- * This module is used through inheritance. It will make available the
- * functions `_blockUser` and `_unblockUser`, which can be used to
- * manage the blocklist in your contract.
  */
 abstract contract ERC20Blocklist is ERC20 {
     /**
@@ -25,58 +22,51 @@ abstract contract ERC20Blocklist is ERC20 {
 
     /**
      * @dev Emitted when a user is blocked.
-     * @param user The address of the user that was blocked.
      */
     event UserBlocked(address indexed user);
 
     /**
      * @dev Emitted when a user is unblocked.
-     * @param user The address of the user that was unblocked.
      */
     event UserUnblocked(address indexed user);
 
     /**
      * @dev The operation failed because the user is blocked.
-     * @param user The address of the user that is blocked.
      */
-    error UserIsBlocked(address user);
+    error ERC20Blocked(address user);
 
     /**
      * @dev The operation failed because the user is not blocked.
      */
-    error UserIsNotBlocked();
 
     /**
      * @dev Blocks a user.
-     * @param user The address of the user to block.
      *
-     * Requirements:
-     *
-     * - The user must not be blocked.
      */
     function _blockUser(address user) internal virtual {
-        if (blocked[user]) revert UserIsBlocked(user);
-        blocked[user] = true;
-        emit UserBlocked(user);
+        if (!blocked[user]) {
+          blocked[user] = true;
+          emit UserBlocked(user);
+        }
     }
 
     /**
      * @dev Unblocks a user.
-     * @param user The address of the user to unblock.
      *
      * Requirements:
      *
      * - The user must be blocked.
      */
     function _unblockUser(address user) internal virtual {
-        if (!blocked[user]) revert UserIsNotBlocked();
-        blocked[user] = false;
-        emit UserUnblocked(user);
+        if (blocked[user]) {
+          blocked[user] = false;
+          emit UserUnblocked(user);
+        }
     }
 
     function _update(address from, address to, uint256 value) internal virtual override {
-        if (blocked[from]) revert UserIsBlocked(from);
-        if (blocked[to]) revert UserIsBlocked(to);
+        if (blocked[from]) revert ERC20Blocked(from);
+        if (blocked[to]) revert ERC20Blocked(to);
         super._update(from, to, value);
     }
 
