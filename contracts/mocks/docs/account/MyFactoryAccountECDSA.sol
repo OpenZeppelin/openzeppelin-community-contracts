dcdae8d6f4c99b9627dc197b2912cf410884fa9d
Ernesto García <ernestognw@gmail.com>
2024-12-09 16:07:22 -0600

    Include signer into account factory hash

--- contracts/mocks/docs/account/MyFactoryAccountECDSA.sol
+++ contracts/mocks/docs/account/MyFactoryAccountECDSA.sol
@@ -26,9 +26,12 @@ contract MyFactoryAccountECDSA {
 
     /// @dev Create clone accounts on demand and return the address. Uses `signer` to initialize the clone.
     function _cloneAndInitialize(bytes32 salt, address signer) internal returns (address) {
-        address predicted = predictAddress(salt);
+        // Scope salt to the signer to avoid front-running the salt with a different signer
+        bytes32 _signerSalt = keccak256(abi.encodePacked(salt, signer));
+
+        address predicted = predictAddress(_signerSalt);
         if (predicted.code.length == 0) {
-            _impl.cloneDeterministic(salt);
+            _impl.cloneDeterministic(_signerSalt);
             MyAccountECDSA(payable(predicted)).initializeSigner(signer);
         }
         return predicted;
