6aa55976cd51548bb9ef1359f2cbef0f274c0c2b
Hadrien Croubois <hadrien.croubois@gmail.com>
2024-12-16 18:17:08 +0100

    doc

--- contracts/account/draft-Account.sol
+++ contracts/account/draft-Account.sol
@@ -7,6 +7,13 @@ import {ERC1155Holder} from "@openzeppelin/contracts/token/ERC1155/utils/ERC1155
 import {ERC7739Signer} from "../utils/cryptography/draft-ERC7739Signer.sol";
 import {AccountCore} from "./draft-AccountCore.sol";
 
+/**
+ * @dev Extention of {AccountCore} with recommanded feature that most account abstraction implementation will want:
+ *
+ * * {ERC721Holder} for ERC-721 token handling
+ * * {ERC1155Holder} for ERC-1155 token handling
+ * * {ERC7739Signer} for ERC-1271 signature support with ERC-7739 replay protection
+ */
 abstract contract Account is AccountCore, ERC721Holder, ERC1155Holder, ERC7739Signer {
     function _rawSignatureValidation(
         bytes32 hash,
