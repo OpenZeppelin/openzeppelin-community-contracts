190b5a578a71099a5b41e991774a17c596425da8
Hadrien Croubois <hadrien.croubois@gmail.com>
2024-12-16 21:58:20 +0100

    doc example for ERC7739 use signers

--- contracts/mocks/docs/utils/cryptography/ERC7739SignerECDSAMock.sol
+++ contracts/mocks/docs/utils/cryptography/ERC7739SignerECDSAMock.sol
@@ -5,19 +5,10 @@ pragma solidity ^0.8.20;
 import {ECDSA} from "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 import {EIP712} from "@openzeppelin/contracts/utils/cryptography/EIP712.sol";
 import {ERC7739Signer} from "../../../../utils/cryptography/draft-ERC7739Signer.sol";
+import {SignerECDSA} from "../../../../utils/cryptography/SignerECDSA.sol";
 
-contract ERC7739SignerECDSAMock is ERC7739Signer {
-    address private immutable _signer;
-
+contract ERC7739SignerECDSAMock is ERC7739Signer, SignerECDSA {
     constructor(address signerAddr) EIP712("ERC7739SignerECDSA", "1") {
-        _signer = signerAddr;
-    }
-
-    function _rawSignatureValidation(
-        bytes32 hash,
-        bytes calldata signature
-    ) internal view virtual override returns (bool) {
-        (address recovered, ECDSA.RecoverError err, ) = ECDSA.tryRecover(hash, signature);
-        return _signer == recovered && err == ECDSA.RecoverError.NoError;
+        _initializeSigner(signerAddr);
     }
 }
