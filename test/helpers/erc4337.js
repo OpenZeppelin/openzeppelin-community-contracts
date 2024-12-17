52784052be76698aa82b4607aed3542f4bfcd895
Hadrien Croubois <hadrien.croubois@gmail.com>
2024-12-17 10:59:30 +0100

    fix entrypoint testing

--- test/helpers/erc4337.js
+++ test/helpers/erc4337.js
@@ -1,30 +1,26 @@
-const { setCode } = require('@nomicfoundation/hardhat-network-helpers');
 const { ethers } = require('hardhat');
-const { UserOperation: UserOperationVanilla } = require('../../lib/@openzeppelin-contracts/test/helpers/erc4337');
-
-const CANONICAL_ENTRYPOINT = '0x0000000071727De22E5E9d8BAf0edAc6f37da032';
+const { UserOperation } = require('@openzeppelin/contracts/test/helpers/erc4337');
+const { deployEntrypoint } = require('@openzeppelin/contracts/test/helpers/erc4337-entrypoint');
 
 /// Global ERC-4337 environment helper.
 class ERC4337Helper {
   constructor(account, params = {}) {
-    this.entrypointAsPromise = ethers.deployContract('EntryPoint');
+    this.entrypointAsPromise = deployEntrypoint();
     this.factoryAsPromise = ethers.deployContract('Create2Mock');
     this.accountContractAsPromise = ethers.getContractFactory(account);
     this.chainIdAsPromise = ethers.provider.getNetwork().then(({ chainId }) => chainId);
-    this.senderCreatorAsPromise = ethers.deployContract('SenderCreator');
     this.params = params;
   }
 
   async wait() {
-    const entrypoint = await this.entrypointAsPromise;
-    await entrypoint.getDeployedCode().then(code => setCode(CANONICAL_ENTRYPOINT, code));
-    this.entrypoint = entrypoint.attach(CANONICAL_ENTRYPOINT);
-    this.entrypointAsPromise = Promise.resolve(this.entrypoint);
+    const { entrypoint, sendercreator } = await this.entrypointAsPromise;
 
+    this.entrypoint = entrypoint;
+    this.senderCreator = sendercreator;
     this.factory = await this.factoryAsPromise;
     this.accountContract = await this.accountContractAsPromise;
     this.chainId = await this.chainIdAsPromise;
-    this.senderCreator = await this.senderCreatorAsPromise;
+
     return this;
   }
 
@@ -61,10 +57,12 @@ class SmartAccount extends ethers.BaseContract {
   }
 
   async createOp(args = {}) {
+    await this.context.wait();
+
     const params = Object.assign({ sender: this }, args);
     // fetch nonce
     if (!params.nonce) {
-      params.nonce = await this.context.entrypointAsPromise.then(entrypoint => entrypoint.getNonce(this, 0));
+      params.nonce = await this.context.entrypoint.getNonce(this, 0);
     }
     // prepare paymaster and data
     if (ethers.isAddressable(params.paymaster)) {
@@ -76,26 +74,27 @@ class SmartAccount extends ethers.BaseContract {
         [params.paymaster, params.paymasterVerificationGasLimit, params.paymasterPostOpGasLimit],
       );
     }
-    return new UserOperation(params);
+
+    return new UserOperationWithContext(params);
   }
 }
 
-class UserOperation extends UserOperationVanilla {
+class UserOperationWithContext extends UserOperation {
   constructor(params) {
     super(params);
     this.context = params.sender.context;
-    this.senderFactory = params.sender.factory;
-    this.senderFactoryData = params.sender.factoryData;
+    this.initCode = {
+      factory: params.sender.factory,
+      factoryData: params.sender.factoryData,
+    };
   }
 
   addInitCode() {
-    this.factory = this.senderFactory;
-    this.factoryData = this.senderFactoryData;
-    return this;
+    return Object.assign(this, this.initCode);
   }
 
   hash() {
-    return super.hash(this.context.entrypoint.target, this.context.chainId);
+    return super.hash(this.context.entrypoint, this.context.chainId);
   }
 }
 
