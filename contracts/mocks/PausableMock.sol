588c681af5a4c60e9fc9cff57b43cdd200ef0d4c
Hadrien Croubois <hadrien.croubois@gmail.com>
2025-01-06 12:29:33 +0100

    deep refactor: inherit @openzeppelin/contracts/utils/Pausable.sol

--- contracts/mocks/PausableMock.sol
+++ contracts/mocks/PausableMock.sol
@@ -2,44 +2,19 @@
 
 pragma solidity ^0.8.20;
 
-import {DefaultPausable} from "../utils/pausability/DefaultPausable.sol";
+import {SafeCast} from "@openzeppelin/contracts/utils/math/SafeCast.sol";
+import {PausableUntil} from "../utils/PausableUntil.sol";
 
-contract PausableMock is DefaultPausable {
-    // solhint-disable-next-line openzeppelin/private-variables
-    bool public drasticMeasureTaken;
-    // solhint-disable-next-line openzeppelin/private-variables
-    uint256 public count;
-
-    constructor() {
-        drasticMeasureTaken = false;
-        count = 0;
+abstract contract PausableUntilMock is PausableUntil {
+    function clock() public view virtual override returns (uint48) {
+        return SafeCast.toUint48(block.timestamp);
     }
 
-    function normalProcess() external whenNotPaused {
-        count++;
+    // solhint-disable-next-line func-name-mixedcase
+    function CLOCK_MODE() public view virtual override returns (string memory) {
+        return "mode=timestamp";
     }
 
-    function drasticMeasure() external whenPaused {
-        drasticMeasureTaken = true;
-    }
-
-    function pause() external {
-        _pause();
-    }
-
-    function unpause() external {
-        _unpause();
-    }
-
-    function pauseUntil(uint256 duration) external {
-        _pauseUntil(uint48(duration));
-    }
-
-    function getPausedUntilDeadline() external view returns (uint256) {
-        return _unpauseDeadline();
-    }
-
-    function getPausedUntilDeadlineAndTimestamp() external view returns (uint256, uint256) {
-        return (_unpauseDeadline(), clock());
-    }
+    function canCallWhenNotPaused() external whenNotPaused {}
+    function canCallWhenPaused() external whenPaused {}
 }
