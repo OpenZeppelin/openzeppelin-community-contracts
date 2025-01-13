6abd1a30e12b5e4b4d36244d37cd565110e9f71e
Hadrien Croubois <hadrien.croubois@gmail.com>
2025-01-13 16:41:50 +0100

    changelog entry

--- contracts/account/Account.sol
+++ contracts/account/Account.sol
@@ -5,15 +5,15 @@ pragma solidity ^0.8.20;
 import {ERC721Holder} from "@openzeppelin/contracts/token/ERC721/utils/ERC721Holder.sol";
 import {ERC1155Holder} from "@openzeppelin/contracts/token/ERC1155/utils/ERC1155Holder.sol";
 import {ERC7739} from "../utils/cryptography/ERC7739.sol";
-import {AccountCore} from "./AccountCore.sol";
 import {ERC7821} from "./extensions/ERC7821.sol";
+import {AccountCore} from "./AccountCore.sol";
 
 /**
  * @dev Extension of {AccountCore} with recommended feature that most account abstraction implementation will want:
  *
- * * {ERC7821} for performing external calls in batches.
  * * {ERC721Holder} and {ERC1155Holder} to accept ERC-712 and ERC-1155 token transfers transfers.
  * * {ERC7739} for ERC-1271 signature support with ERC-7739 replay protection
+ * * {ERC7821} for performing external calls in batches.
  *
  * NOTE: To use this contract, the {ERC7739-_rawSignatureValidation} function must be
  * implemented using a specific signature verification algorithm. See {SignerECDSA}, {SignerP256} or {SignerRSA}.
