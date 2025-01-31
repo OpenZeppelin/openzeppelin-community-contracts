36ca021544d93f48d6d86cc5145ec08a661ca517
Hadrien Croubois <hadrien.croubois@gmail.com>
2025-01-31 17:54:35 +0100

    Remove unecessary imports

--- contracts/mocks/account/paymaster/PaymasterSignerECDSAMock.sol
+++ contracts/mocks/account/paymaster/PaymasterSignerECDSAMock.sol
@@ -3,14 +3,10 @@
 pragma solidity ^0.8.20;
 
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
-import {PackedUserOperation} from "@openzeppelin/contracts/interfaces/draft-IERC4337.sol";
-import {ERC4337Utils} from "@openzeppelin/contracts/account/utils/draft-ERC4337Utils.sol";
 import {PaymasterSigner, EIP712} from "../../../account/paymaster/PaymasterSigner.sol";
 import {SignerECDSA} from "../../../utils/cryptography/SignerECDSA.sol";
 
 contract PaymasterSignerECDSAMock is PaymasterSigner, SignerECDSA, Ownable {
-    using ERC4337Utils for *;
-
     constructor(address signerAddr, address withdrawer) EIP712("MyPaymasterECDSASigner", "1") Ownable(withdrawer) {
         _setSigner(signerAddr);
     }
