5f5e785d9c043e13b091cceb44a72a16e56ef05c
Jérémy James Toussaint <33313130+jeremyjams@users.noreply.github.com>
2025-02-11 21:16:50 +0100

    Bump version pragma of ERC6900Utils for consistency

--- contracts/account/utils/ERC6900Utils.sol
+++ contracts/account/utils/ERC6900Utils.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.8.20;
+pragma solidity ^0.8.24;
 
 import {ValidationConfig, ModuleEntity, ValidationFlags, HookConfig, ExecutionManifest, ManifestExecutionHook, ManifestExecutionFunction, Call} from "../../interfaces/IERC6900.sol";
 import {Bytes} from "@openzeppelin/contracts/utils/Bytes.sol";
