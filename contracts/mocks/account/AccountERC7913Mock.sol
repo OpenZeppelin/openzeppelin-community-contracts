6a88076be1bde0dbb91685a2bbcec9db24f21470
Hadrien Croubois <hadrien.croubois@gmail.com>
2025-03-29 21:46:57 +0100

    pragma

--- contracts/mocks/account/AccountERC7913Mock.sol
+++ contracts/mocks/account/AccountERC7913Mock.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.8.20;
+pragma solidity ^0.8.24;
 
 import {Account} from "../../account/Account.sol";
 import {ERC7821} from "../../account/extensions/ERC7821.sol";
