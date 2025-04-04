4ec7569149668922484b3eabee01ec736ddcd0da
ernestognw <ernestognw@gmail.com>
2025-04-04 15:55:45 -0600

    Apply pragma to SignerZKEmail too

--- contracts/utils/cryptography/SignerZKEmail.sol
+++ contracts/utils/cryptography/SignerZKEmail.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.8.20;
+pragma solidity ^0.8.24;
 
 import {IDKIMRegistry} from "@zk-email/contracts/DKIMRegistry.sol";
 import {IVerifier, EmailProof} from "@zk-email/email-tx-builder/interfaces/IVerifier.sol";
