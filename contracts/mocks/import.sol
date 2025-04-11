f26d26b1f25acdd5cedd0d464c145b3cbf947114
ernestognw <ernestognw@gmail.com>
2025-04-10 18:53:58 -0600

    Add Hardhat tests

--- contracts/mocks/import.sol
+++ contracts/mocks/import.sol
@@ -3,3 +3,4 @@
 pragma solidity ^0.8.20;
 
 import {UpgradeableBeacon} from "@openzeppelin/contracts/proxy/beacon/UpgradeableBeacon.sol";
+import {ECDSAOwnedDKIMRegistry} from "@zk-email/email-tx-builder/utils/ECDSAOwnedDKIMRegistry.sol";
