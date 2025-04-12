a1d07fe6e3880e39f9a492502cb8517c7e384add
ernestognw <ernestognw@gmail.com>
2025-04-12 13:56:51 -0600

    Improve docs

--- contracts/utils/cryptography/SignerERC7913.sol
+++ contracts/utils/cryptography/SignerERC7913.sol
@@ -6,10 +6,13 @@ import {AbstractSigner} from "./AbstractSigner.sol";
 import {ERC7913Utils} from "./ERC7913Utils.sol";
 
 /**
- * @dev Implementation of {AbstractSigner} that supports ERC-7913 signers.
+ * @dev Implementation of {AbstractSigner} using
+ * https://eips.ethereum.org/EIPS/eip-7913[ERC-7913] signature verification.
  *
- * For {Account} usage, an {_setSigner} function is provided to set the ERC-7913 formatted {signer}.
- * Doing so it's easier for a factory, whose likely to use initializable clones of this contract.
+ * For {Account} usage, a {_setSigner} function is provided to set the ERC-7913 formatted {signer}.
+ * Doing so is easier for a factory, who is likely to use initializable clones of this contract.
+ *
+ * The signer is a `bytes` object that concatenates a verifier address and a key: `verifier || key`.
  *
  * Example of usage:
  *
@@ -17,28 +20,30 @@ import {ERC7913Utils} from "./ERC7913Utils.sol";
  * contract MyAccountERC7913 is Account, SignerERC7913, Initializable {
  *     constructor() EIP712("MyAccountERC7913", "1") {}
  *
- *     function initialize(bytes memory signer) public initializer {
- *       _setSigner(signer);
+ *     function initialize(bytes memory signer_) public initializer {
+ *       _setSigner(signer_);
  *     }
  * }
  * ```
  *
- * IMPORTANT: Avoiding to call {_setSigner} either during construction (if used standalone)
+ * IMPORTANT: Failing to call {_setSigner} either during construction (if used standalone)
  * or during initialization (if used as a clone) may leave the signer either front-runnable or unusable.
  */
 
 abstract contract SignerERC7913 is AbstractSigner {
     bytes private _signer;
 
+    /// @dev Sets the signer (i.e. `verifier || key`) with an ERC-7913 formatted signer.
     function _setSigner(bytes memory signer_) internal {
         _signer = signer_;
     }
 
+    /// @dev Return the ERC-7913 signer (i.e. `verifier || key`).
     function signer() public view virtual returns (bytes memory) {
         return _signer;
     }
 
-    /// @inheritdoc AbstractSigner
+    /// @dev Verifies a signature using {ERC7913Utils.isValidSignatureNow} with {signer}, `hash` and `signature`.
     function _rawSignatureValidation(
         bytes32 hash,
         bytes calldata signature
