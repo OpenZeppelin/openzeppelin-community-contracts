221c7430ce302e1098acc793d272060d6b5484e0
ernestognw <ernestognw@gmail.com>
2025-04-11 22:46:05 -0600

    simplify

--- contracts/utils/cryptography/ERC7913Utils.sol
+++ contracts/utils/cryptography/ERC7913Utils.sol
@@ -13,22 +13,6 @@ import {IERC7913SignatureVerifier} from "../../interfaces/IERC7913.sol";
  * to support signature verification for keys that do not have an Ethereum address of their own
  * as with ERC-1271.
  *
- * ERC-7913 enables signature verification for non-Ethereum cryptographic keys such as:
- * - Non-native cryptographic curves (e.g., secp256r1, RSA)
- * - Hardware devices
- * - Email addresses
- * - JWT tokens from Web2 services
- *
- * A signer is represented as a `bytes` object that is the concatenation of a verifier address
- * and optionally a key: `verifier || key`. The verifier is a smart contract that implements
- * {IERC7913SignatureVerifier} and is responsible for verifying signatures for a specific type
- * of key.
- *
- * This library provides backward compatibility with existing systems:
- * - For EOAs: The signer is just the EOA address (20 bytes)
- * - For ERC-1271 contracts: The signer is just the contract address (20 bytes)
- * - For ERC-7913 keys: The signer is the verifier address concatenated with the key
- *
  * See https://eips.ethereum.org/EIPS/eip-7913[ERC-7913].
  */
 library ERC7913Utils {
@@ -42,11 +26,6 @@ library ERC7913Utils {
      * - If `signer.length < 20`: verification fails
      * - If `signer.length == 20`: verification is done using {SignatureChecker}
      * - Otherwise: verification is done using {IERC7913SignatureVerifier}
-     *
-     * @param signer The signer bytes (verifier address || key)
-     * @param hash The hash of the message being signed
-     * @param signature The signature to verify
-     * @return bool Whether the signature is valid
      */
     function isValidSignatureNow(
         bytes calldata signer,
