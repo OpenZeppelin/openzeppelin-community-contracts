4482164a50b15f960f79cba3f3f66eabedea1632
ernestognw <ernestognw@gmail.com>
2025-05-09 00:15:40 -0600

    nit

--- contracts/mocks/account/AccountWebAuthnMock.sol
+++ contracts/mocks/account/AccountWebAuthnMock.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.8.20;
+pragma solidity ^0.8.24;
 
 import {Account} from "../../account/Account.sol";
 import {ERC721Holder} from "@openzeppelin/contracts/token/ERC721/utils/ERC721Holder.sol";
