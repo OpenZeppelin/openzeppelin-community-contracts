e312d0bc9aee447fb2ccaedc0ff6cd4ca2463001
ernestognw <ernestognw@gmail.com>
2025-05-12 20:28:51 -0600

    Moar tests

---
+++ contracts/mocks/account/modules/ERC7579MultisigExecutorMock.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.27;
+
+import {ERC7579Executor} from "../../../account/modules/ERC7579Executor.sol";
+import {ERC7579Multisig} from "../../../account/modules/ERC7579Multisig.sol";
+import {MODULE_TYPE_EXECUTOR, IERC7579Hook} from "@openzeppelin/contracts/interfaces/draft-IERC7579.sol";
+
+abstract contract ERC7579MultisigExecutorMock is ERC7579Executor, ERC7579Multisig {}
