9f815d9dde716470c650fb33dc5fa182bffda2cb
ernestognw <ernestognw@gmail.com>
2025-05-17 18:21:17 -0600

    Fix compilation

--- contracts/mocks/docs/account/modules/MyERC7579DelayedSocialRecovery.sol
+++ contracts/mocks/docs/account/modules/MyERC7579DelayedSocialRecovery.sol
@@ -3,6 +3,8 @@
 pragma solidity ^0.8.27;
 
 import {EIP712} from "@openzeppelin/contracts/utils/cryptography/EIP712.sol";
+import {ERC7579Executor} from "../../../../account/modules/ERC7579Executor.sol";
+import {ERC7579Validator} from "../../../../account/modules/ERC7579Validator.sol";
 import {Calldata} from "@openzeppelin/contracts/utils/Calldata.sol";
 import {ERC7579DelayedExecutor} from "../../../../account/modules/ERC7579DelayedExecutor.sol";
 import {ERC7579Multisig} from "../../../../account/modules/ERC7579Multisig.sol";
@@ -10,6 +12,10 @@ import {ERC7579Multisig} from "../../../../account/modules/ERC7579Multisig.sol";
 abstract contract MyERC7579DelayedSocialRecovery is EIP712, ERC7579DelayedExecutor, ERC7579Multisig {
     bytes32 private constant RECOVER_TYPEHASH =
         keccak256("Recover(address account,bytes32 salt,bytes32 mode,bytes executionCalldata)");
+
+    function isModuleType(uint256 moduleTypeId) public pure override(ERC7579Executor, ERC7579Validator) returns (bool) {
+        return ERC7579Executor.isModuleType(moduleTypeId) || ERC7579Executor.isModuleType(moduleTypeId);
+    }
 
     // Data encoding: [uint16(executorArgsLength), executorArgs, uint16(multisigArgsLength), multisigArgs]
     function onInstall(bytes calldata data) public override(ERC7579DelayedExecutor, ERC7579Multisig) {
@@ -37,9 +43,7 @@ abstract contract MyERC7579DelayedSocialRecovery is EIP712, ERC7579DelayedExecut
         uint16 executionCalldataLength = uint16(uint256(bytes32(data[0:2]))); // First 2 bytes are the length
         bytes calldata executionCalldata = data[2:2 + executionCalldataLength]; // Next bytes are the calldata
         bytes calldata signature = data[2 + executionCalldataLength:]; // Remaining bytes are the signature
-        require(
-            _validateMultisignature(account, _getExecuteTypeHash(account, salt, mode, executionCalldata), signature)
-        );
+        require(_rawERC7579Validation(account, _getExecuteTypeHash(account, salt, mode, executionCalldata), signature));
     }
 
     function _getExecuteTypeHash(
--- contracts/mocks/docs/account/modules/MyERC7579SocialRecovery.sol
+++ contracts/mocks/docs/account/modules/MyERC7579SocialRecovery.sol
@@ -5,11 +5,16 @@ pragma solidity ^0.8.27;
 import {Nonces} from "@openzeppelin/contracts/utils/Nonces.sol";
 import {EIP712} from "@openzeppelin/contracts/utils/cryptography/EIP712.sol";
 import {ERC7579Executor} from "../../../../account/modules/ERC7579Executor.sol";
+import {ERC7579Validator} from "../../../../account/modules/ERC7579Validator.sol";
 import {ERC7579Multisig} from "../../../../account/modules/ERC7579Multisig.sol";
 
 abstract contract MyERC7579SocialRecovery is EIP712, ERC7579Executor, ERC7579Multisig, Nonces {
     bytes32 private constant RECOVER_TYPEHASH =
         keccak256("Recover(address account,bytes32 salt,uint256 nonce,bytes32 mode,bytes executionCalldata)");
+
+    function isModuleType(uint256 moduleTypeId) public pure override(ERC7579Executor, ERC7579Validator) returns (bool) {
+        return ERC7579Executor.isModuleType(moduleTypeId) || ERC7579Executor.isModuleType(moduleTypeId);
+    }
 
     // Data encoding: [uint16(executionCalldataLength), executionCalldata, signature]
     function _validateExecution(
@@ -21,9 +26,7 @@ abstract contract MyERC7579SocialRecovery is EIP712, ERC7579Executor, ERC7579Mul
         uint16 executionCalldataLength = uint16(uint256(bytes32(data[0:2]))); // First 2 bytes are the length
         bytes calldata executionCalldata = data[2:2 + executionCalldataLength]; // Next bytes are the calldata
         bytes calldata signature = data[2 + executionCalldataLength:]; // Remaining bytes are the signature
-        require(
-            _validateMultisignature(account, _getExecuteTypeHash(account, salt, mode, executionCalldata), signature)
-        );
+        require(_rawERC7579Validation(account, _getExecuteTypeHash(account, salt, mode, executionCalldata), signature));
         return executionCalldata;
     }
 
