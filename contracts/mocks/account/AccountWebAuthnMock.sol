1bfa07315c657bfbc88113a3f1fcaa48bce85edb
ernestognw <ernestognw@gmail.com>
2025-06-04 14:10:15 -0600

    Fix another import

--- contracts/mocks/account/AccountWebAuthnMock.sol
+++ contracts/mocks/account/AccountWebAuthnMock.sol
@@ -5,7 +5,7 @@ pragma solidity ^0.8.24;
 import {Account} from "../../account/Account.sol";
 import {ERC721Holder} from "@openzeppelin/contracts/token/ERC721/utils/ERC721Holder.sol";
 import {ERC1155Holder} from "@openzeppelin/contracts/token/ERC1155/utils/ERC1155Holder.sol";
-import {ERC7739} from "../../utils/cryptography/ERC7739.sol";
+import {ERC7739} from "../../utils/cryptography/signers/ERC7739.sol";
 import {ERC7821} from "../../account/extensions/ERC7821.sol";
 import {SignerWebAuthn} from "../../utils/cryptography/signers/SignerWebAuthn.sol";
 
