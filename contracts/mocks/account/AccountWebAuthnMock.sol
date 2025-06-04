0e8a7001ea6199b22edfe75b0a7e10fcfaec59d4
ernestognw <ernestognw@gmail.com>
2025-06-04 14:08:41 -0600

    Fix import

--- contracts/mocks/account/AccountWebAuthnMock.sol
+++ contracts/mocks/account/AccountWebAuthnMock.sol
@@ -7,7 +7,7 @@ import {ERC721Holder} from "@openzeppelin/contracts/token/ERC721/utils/ERC721Hol
 import {ERC1155Holder} from "@openzeppelin/contracts/token/ERC1155/utils/ERC1155Holder.sol";
 import {ERC7739} from "../../utils/cryptography/ERC7739.sol";
 import {ERC7821} from "../../account/extensions/ERC7821.sol";
-import {SignerWebAuthn} from "../../utils/cryptography/SignerWebAuthn.sol";
+import {SignerWebAuthn} from "../../utils/cryptography/signers/SignerWebAuthn.sol";
 
 abstract contract AccountWebAuthnMock is Account, SignerWebAuthn, ERC7739, ERC7821, ERC721Holder, ERC1155Holder {
     constructor(bytes32 qx, bytes32 qy) {
