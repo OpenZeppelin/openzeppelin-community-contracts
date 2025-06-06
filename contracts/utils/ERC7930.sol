3a8412842fc31376399dc5264c5dba8c18f324e7
Hadrien Croubois <hadrien.croubois@gmail.com>
2025-06-06 15:29:01 +0200

    Add an ERC-7930 parsing library for ERC-7786

---
+++ contracts/utils/ERC7930.sol
@@ -0,0 +1,117 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.24;
+
+import {Bytes} from "@openzeppelin/contracts/utils/Bytes.sol";
+import {Calldata} from "@openzeppelin/contracts/utils/Calldata.sol";
+import {SafeCast} from "@openzeppelin/contracts/utils/math/SafeCast.sol";
+
+// 0x00010000010114D8DA6BF26964AF9D7EED9E03E53415D37AA96045
+// 0x000100022045296998a6f8e2a784db5d9f95e18fc23f70441a1039446801089879b08c7ef02005333498d5aea4ae009585c43f7b8c30df8e70187d4a713d134f977fc8dfe0b5
+// 0x000100000014D8DA6BF26964AF9D7EED9E03E53415D37AA96045
+// 0x000100022045296998a6f8e2a784db5d9f95e18fc23f70441a1039446801089879b08c7ef000
+// 0x0001000002A4B114D8DA6BF26964AF9D7EED9E03E53415D37AA96045
+
+library ERC7930 {
+    using SafeCast for uint256;
+    using Bytes for bytes;
+
+    error ERC7930ParsingError(bytes);
+
+    function formatV1(
+        bytes2 chainType,
+        bytes memory chainReference,
+        bytes memory addr
+    ) internal pure returns (bytes memory) {
+        return
+            abi.encodePacked(
+                bytes2(0x0001),
+                chainType,
+                chainReference.length.toUint8(),
+                chainReference,
+                addr.length.toUint8(),
+                addr
+            );
+    }
+
+    function parseV1(
+        bytes memory self
+    ) internal pure returns (bytes2 chainType, bytes memory chainReference, bytes memory addr) {
+        bool success;
+        (success, chainType, chainReference, addr) = tryParseV1(self);
+        require(success, ERC7930ParsingError(self));
+    }
+
+    function parseV1Calldata(
+        bytes calldata self
+    ) internal pure returns (bytes2 chainType, bytes calldata chainReference, bytes calldata addr) {
+        bool success;
+        (success, chainType, chainReference, addr) = tryParseV1Calldata(self);
+        require(success, ERC7930ParsingError(self));
+    }
+
+    function tryParseV1(
+        bytes memory self
+    ) internal pure returns (bool success, bytes2 chainType, bytes memory chainReference, bytes memory addr) {
+        unchecked {
+            success = true;
+            if (self.length < 0x06) return (false, 0x0000, _emptyBytesMemory(), _emptyBytesMemory());
+
+            bytes2 version = _readBytes2(self, 0x00);
+            if (version != bytes2(0x0001)) return (false, 0x0000, _emptyBytesMemory(), _emptyBytesMemory());
+            chainType = _readBytes2(self, 0x02);
+
+            uint8 chainReferenceLength = uint8(self[0x04]);
+            if (self.length < 0x06 + chainReferenceLength)
+                return (false, 0x0000, _emptyBytesMemory(), _emptyBytesMemory());
+            chainReference = self.slice(0x05, 0x05 + chainReferenceLength);
+
+            uint8 addrLength = uint8(self[0x05 + chainReferenceLength]);
+            if (self.length < 0x06 + chainReferenceLength + addrLength)
+                return (false, 0x0000, _emptyBytesMemory(), _emptyBytesMemory());
+            addr = self.slice(0x06 + chainReferenceLength, 0x06 + chainReferenceLength + addrLength);
+        }
+    }
+
+    function tryParseV1Calldata(
+        bytes calldata self
+    ) internal pure returns (bool success, bytes2 chainType, bytes calldata chainReference, bytes calldata addr) {
+        unchecked {
+            success = true;
+            if (self.length < 0x06) return (false, 0x0000, Calldata.emptyBytes(), Calldata.emptyBytes());
+
+            bytes2 version = _readBytes2(self, 0x00);
+            if (version != bytes2(0x0001)) return (false, 0x0000, Calldata.emptyBytes(), Calldata.emptyBytes());
+            chainType = _readBytes2(self, 0x02);
+
+            uint8 chainReferenceLength = uint8(self[0x04]);
+            if (self.length < 0x06 + chainReferenceLength)
+                return (false, 0x0000, Calldata.emptyBytes(), Calldata.emptyBytes());
+            chainReference = self[0x05:0x05 + chainReferenceLength];
+
+            uint8 addrLength = uint8(self[0x05 + chainReferenceLength]);
+            if (self.length < 0x06 + chainReferenceLength + addrLength)
+                return (false, 0x0000, Calldata.emptyBytes(), Calldata.emptyBytes());
+            addr = self[0x06 + chainReferenceLength:0x06 + chainReferenceLength + addrLength];
+        }
+    }
+
+    function _readBytes2(bytes memory buffer, uint256 offset) private pure returns (bytes2 value) {
+        // This is not memory safe in the general case, but all calls to this private function are within bounds.
+        assembly ("memory-safe") {
+            value := shl(240, shr(240, mload(add(add(buffer, 0x20), offset))))
+        }
+    }
+
+    function _readBytes2Calldata(bytes calldata buffer, uint256 offset) private pure returns (bytes2 value) {
+        assembly ("memory-safe") {
+            value := shl(240, shr(240, calldataload(add(buffer.offset, offset))))
+        }
+    }
+
+    function _emptyBytesMemory() private pure returns (bytes memory result) {
+        assembly ("memory-safe") {
+            result := 0x60 // mload(0x60) is always 0
+        }
+    }
+}
---
+++ test/utils/ERC7930.test.js
@@ -0,0 +1,66 @@
+const { ethers } = require('hardhat');
+const { expect } = require('chai');
+const { loadFixture } = require('@nomicfoundation/hardhat-network-helpers');
+
+async function fixture() {
+  const mock = await ethers.deployContract('$ERC7930');
+  return { mock };
+}
+
+describe('ERC7390', function () {
+  before(async function () {
+    Object.assign(this, await loadFixture(fixture));
+  });
+
+  it('Example 1: Ethereum mainnet address', async function () {
+    await expect(
+      this.mock.$parseV1('0x00010000010114D8DA6BF26964AF9D7EED9E03E53415D37AA96045'),
+    ).to.eventually.deep.equal([
+      '0x0000', // eip155
+      '0x01', // mainnet
+      '0xd8dA6BF26964aF9D7eEd9e03E53415D37aA96045', // 20 bytes of the ethereum address
+    ]);
+  });
+
+  it('Example 2: Solana mainnet address', async function () {
+    await expect(
+      this.mock.$parseV1(
+        '0x000100022045296998a6f8e2a784db5d9f95e18fc23f70441a1039446801089879b08c7ef02005333498d5aea4ae009585c43f7b8c30df8e70187d4a713d134f977fc8dfe0b5',
+      ),
+    ).to.eventually.deep.equal([
+      '0x0002', // solana
+      '0x45296998a6f8e2a784db5d9f95e18fc23f70441a1039446801089879b08c7ef0', // 32 bytes of the solana genesis block
+      '0x05333498d5aea4ae009585c43f7b8c30df8e70187d4a713d134f977fc8dfe0b5', // 32 bytes of the solana address
+    ]);
+  });
+
+  it('Example 3: EVM address without chainid', async function () {
+    await expect(this.mock.$parseV1('0x000100000014D8DA6BF26964AF9D7EED9E03E53415D37AA96045')).to.eventually.deep.equal(
+      [
+        '0x0000', // eip155
+        '0x', // no chainid
+        '0xd8dA6BF26964aF9D7eEd9e03E53415D37aA96045', // 20 bytes of the ethereum address
+      ],
+    );
+  });
+
+  it('Example 4: Solana mainnet network, no address', async function () {
+    await expect(
+      this.mock.$parseV1('0x000100022045296998a6f8e2a784db5d9f95e18fc23f70441a1039446801089879b08c7ef000'),
+    ).to.eventually.deep.equal([
+      '0x0002', // solana
+      '0x45296998a6f8e2a784db5d9f95e18fc23f70441a1039446801089879b08c7ef0', // 32 bytes of the solana genesis block
+      '0x', // no address
+    ]);
+  });
+
+  it('Example 5: Arbitrum One address', async function () {
+    await expect(
+      this.mock.$parseV1('0x0001000002A4B114D8DA6BF26964AF9D7EED9E03E53415D37AA96045'),
+    ).to.eventually.deep.equal([
+      '0x0000', // eip155
+      '0xA4B1', // arbitrum one chainid (42161)
+      '0xd8dA6BF26964aF9D7eEd9e03E53415D37aA96045', // 20 bytes of the ethereum address
+    ]);
+  });
+});
