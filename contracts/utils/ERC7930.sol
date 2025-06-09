9e185b04c21f8da2d35b4cdfe5cee7ac76e3a5b4
Hadrien Croubois <hadrien.croubois@gmail.com>
2025-06-09 18:02:09 +0200

    Update ERC7930.sol

--- contracts/utils/ERC7930.sol
+++ contracts/utils/ERC7930.sol
@@ -7,12 +7,6 @@ import {Calldata} from "@openzeppelin/contracts/utils/Calldata.sol";
 import {Math} from "@openzeppelin/contracts/utils/math/Math.sol";
 import {SafeCast} from "@openzeppelin/contracts/utils/math/SafeCast.sol";
 
-// 0x00010000010114D8DA6BF26964AF9D7EED9E03E53415D37AA96045
-// 0x000100022045296998a6f8e2a784db5d9f95e18fc23f70441a1039446801089879b08c7ef02005333498d5aea4ae009585c43f7b8c30df8e70187d4a713d134f977fc8dfe0b5
-// 0x000100000014D8DA6BF26964AF9D7EED9E03E53415D37AA96045
-// 0x000100022045296998a6f8e2a784db5d9f95e18fc23f70441a1039446801089879b08c7ef000
-// 0x0001000002A4B114D8DA6BF26964AF9D7EED9E03E53415D37AA96045
-
 library ERC7930 {
     using SafeCast for uint256;
     using Bytes for bytes;
