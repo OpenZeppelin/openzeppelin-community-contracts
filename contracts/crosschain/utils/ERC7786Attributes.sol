d3b39cd9f13dec5dca0e0a4f18987abcf349e8ff
Hadrien Croubois <hadrien.croubois@gmail.com>
2025-07-25 22:59:58 +0200

    test Erc7786Attributes

--- contracts/crosschain/utils/ERC7786Attributes.sol
+++ contracts/crosschain/utils/ERC7786Attributes.sol
@@ -7,27 +7,25 @@ import {IERC7786Attributes} from "../../interfaces/IERC7786Attributes.sol";
 library ERC7786Attributes {
     function tryDecodeRequestRelay(
         bytes memory attribute
-    ) internal returns (bool success, uint256 value, uint256 gasLimit, address refundRecipient) {
+    ) internal pure returns (bool success, uint256 value, uint256 gasLimit, address refundRecipient) {
         success = bytes4(attribute) == IERC7786Attributes.requestRelay.selector && attribute.length >= 0x64;
-        if (success) {
-            assembly ("memory-safe") {
-                value := mload(add(attribute, 0x24))
-                gasLimit := mload(add(attribute, 0x44))
-                refundRecipient := mload(add(attribute, 0x64))
-            }
+
+        assembly ("memory-safe") {
+            value := mul(success, mload(add(attribute, 0x24)))
+            gasLimit := mul(success, mload(add(attribute, 0x44)))
+            refundRecipient := mul(success, mload(add(attribute, 0x64)))
         }
     }
 
     function tryDecodeRequestRelayCalldata(
         bytes calldata attribute
-    ) internal returns (bool success, uint256 value, uint256 gasLimit, address refundRecipient) {
+    ) internal pure returns (bool success, uint256 value, uint256 gasLimit, address refundRecipient) {
         success = bytes4(attribute) == IERC7786Attributes.requestRelay.selector && attribute.length >= 0x64;
-        if (success) {
-            assembly ("memory-safe") {
-                value := calldataload(add(attribute.offset, 0x04))
-                gasLimit := calldataload(add(attribute.offset, 0x24))
-                refundRecipient := calldataload(add(attribute.offset, 0x44))
-            }
+
+        assembly ("memory-safe") {
+            value := mul(success, calldataload(add(attribute.offset, 0x04)))
+            gasLimit := mul(success, calldataload(add(attribute.offset, 0x24)))
+            refundRecipient := mul(success, calldataload(add(attribute.offset, 0x44)))
         }
     }
 }
