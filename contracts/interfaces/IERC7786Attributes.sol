541c47b24c4b522d49c7405e7be435e1c3968d3e
Hadrien Croubois <hadrien.croubois@gmail.com>
2025-07-29 12:06:19 +0200

    Apply suggestions from code review

--- contracts/interfaces/IERC7786Attributes.sol
+++ contracts/interfaces/IERC7786Attributes.sol
@@ -3,7 +3,7 @@
 pragma solidity ^0.8.4;
 
 /**
- * @dev Standard attributes for ERC-7786. These attributes may be standardized in different ERC.
+ * @dev Standard attributes for ERC-7786. These attributes may be standardized in different ERCs.
  */
 interface IERC7786Attributes {
     function requestRelay(uint256 value, uint256 gasLimit, address refundRecipient) external;
