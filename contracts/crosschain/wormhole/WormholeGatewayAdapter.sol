a7ee3908fabe9fab2b88db34b89412a09c0dae89
James Toussaint <33313130+james-toussaint@users.noreply.github.com>
2026-09-02 14:29:40 +0200

    Increase transpilation compatibility

--- contracts/crosschain/wormhole/WormholeGatewayAdapter.sol
+++ contracts/crosschain/wormhole/WormholeGatewayAdapter.sol
@@ -24,17 +24,6 @@ contract WormholeGatewayAdapter is IERC7786GatewaySource, IWormholeReceiver, Own
     using BitMaps for BitMaps.BitMap;
     using InteroperableAddress for bytes;
 
-    IWormholeRelayer internal immutable _wormholeRelayer;
-    uint16 internal immutable _wormholeChainId;
-    uint24 private constant EVM_ID_FLAG = 1 << 16;
-
-    // Remote gateway.
-    mapping(uint256 chainId => address) private _remoteGateways;
-
-    // Chain equivalence ChainId <> Wormhole
-    mapping(uint256 chainId => uint24 wormholeId) private _chainIdToWormhole;
-    mapping(uint16 wormholeId => uint256 chainId) private _wormholeToChainId;
-
     // Message temporary representation, waiting for gas payment in requestRelay
     struct PendingMessage {
         bool pending;
@@ -43,6 +32,17 @@ contract WormholeGatewayAdapter is IERC7786GatewaySource, IWormholeReceiver, Own
         bytes recipient;
         bytes payload;
     }
+
+    uint24 private constant EVM_ID_FLAG = 1 << 16;
+    IWormholeRelayer private immutable _wormholeRelayer;
+    uint16 private immutable _wormholeChainId;
+
+    // Remote gateway.
+    mapping(uint256 chainId => address) private _remoteGateways;
+
+    // Chain equivalence ChainId <> Wormhole
+    mapping(uint256 chainId => uint24 wormholeId) private _chainIdToWormhole;
+    mapping(uint16 wormholeId => uint256 chainId) private _wormholeToChainId;
 
     uint256 private _lastMsgId;
     mapping(bytes32 sendId => PendingMessage) private _pending;
@@ -71,7 +71,7 @@ contract WormholeGatewayAdapter is IERC7786GatewaySource, IWormholeReceiver, Own
     error MessageAlreadyExecuted(uint256 chainId, bytes32 outboxId);
 
     modifier onlyWormholeRelayer() {
-        require(msg.sender == address(_wormholeRelayer), UnauthorizedCaller(msg.sender));
+        require(msg.sender == relayer(), UnauthorizedCaller(msg.sender));
         _;
     }
 
@@ -84,6 +84,11 @@ contract WormholeGatewayAdapter is IERC7786GatewaySource, IWormholeReceiver, Own
     /// @dev Returns the local Wormhole relayer
     function relayer() public view virtual returns (address) {
         return address(_wormholeRelayer);
+    }
+
+    /// @dev Returns the Wormhole chain id.
+    function wormholeChainId() public view virtual returns (uint16) {
+        return _wormholeChainId;
     }
 
     /// @dev Returns whether a binary interoperable chain id is supported.
--- test/crosschain/wormhole/WormholeGatewayAdapter.test.js
+++ test/crosschain/wormhole/WormholeGatewayAdapter.test.js
@@ -39,6 +39,7 @@ describe('WormholeGatewayAdapter', function () {
 
   it('initial setup', async function () {
     await expect(this.gatewayA.relayer()).to.eventually.equal(this.wormhole);
+    await expect(this.gatewayA.wormholeChainId()).to.eventually.equal(this.wormholeChainId);
     await expect(this.gatewayA.getChainId(this.wormholeChainId)).to.eventually.equal(this.chain.reference);
     await expect(this.gatewayA.getWormholeChain(ethers.Typed.bytes(this.chain.erc7930))).to.eventually.equal(
       this.wormholeChainId,
